a6ad66c7eb95309501ab3e4cbd0ca4bbe4cfff81
hong-s-hyeon <116802198+hong-s-hyeon@users.noreply.github.com>
2022-11-02 16:52:10 +0900

    홍승현#11.02/16:50
    - app.py 기본 내용 저장
    - bot.py 기본 내용 저장
    - import 하는 과정에서 발생하였던
      모든 명칭 관련 이슈 처리(80%)

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, jsonify, abort
+import socket
+import json
+ 
+
+# 챗봇 엔진 서버 접속 정보
+# 이전에 만든 챗봇 엔진 서버에서 설정한 포트를 사용해야 한다
+host = "127.0.0.1"  # 챗봇 엔진 서버 IP 주소
+port = 5050  # 챗봇 엔진 서버 통신 포트
+ 
+# Flask 어플리케이션
+app = Flask(__name__)
+
+from flask_cors import CORS
+CORS(app)
+
+# 챗봇 엔진 서버와 통신 (소켓 통신!)
+# 질의를 전송하고, 답변데이터를 수신한 경우 JSON 문자열을 dict 객체로 변환
+def get_answer_from_engine(bottype, query):    
+        # 챗봇 엔진 서버 연결
+    mySocket = socket.socket()
+    mySocket.connect((host, port))            #서버를 요청 해당 주소쪽으로~
+ 
+    # 챗봇 엔진 질의 요청
+    json_data = {                              # 해당 서버로 보낼거~~~~우리 서버(api서버말고)
+        'Query': query,
+        'BotType': bottype
+    }
+    message = json.dumps(json_data)
+    mySocket.send(message.encode())
+ 
+    # 챗봇 엔진 답변 출력
+    data = mySocket.recv(2048).decode()
+    ret_data = json.loads(data)
+ 
+    # 챗봇 엔진 서버 연결 소켓 닫기
+    mySocket.close()
+ 
+    return ret_data
+ 
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return ('hello')
+
+#챗봇 엔진 query 전송 API
+@app.route("/query/<bot_type>", methods=['POST'])
+def query(bot_type):
+
+    body = request.get_json()
+    ret = {}
+    try:
+        if bot_type == 'TEST':
+            # TODO: 챗봇엔진에 소켓통신하여 query 를 보내고 답을 받아오기
+            ret = get_answer_from_engine(bottype=bot_type, query=body['query'])
+            print("ret: ", ret, "type: ", type(ret))
+            return jsonify(ret)
+        elif bot_type =="KAKAO":
+            #카카오 처리
+            pass
+        elif bot_type == "NAVER":
+            # 네이버톡 처리
+            pass
+        else:
+            # 정의되지 않은 boy type의 경우 404 에러
+            abort(404)
+
+    except Exception as ex:
+        # 오류 발생시 500에러
+        abort(500)    
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.10', port=5000, debug=True)
+
+
+# api 서버가 이런 역할을 해야한다~! 이런 마인드로 여길 봐라~!!~!~!~!~!~!~!
---
+++ bot.py
@@ -0,0 +1,141 @@
+'''
+thread : 쓰레드
+리퀘스트가 들어오면 별도의 쓰레드를 활용한다
+
+# 파이썬은 원래 기본적으로 한번에 하나의 쓰레드밖에 실행 못한다
+# 'threading 모듈'을 통해 (내부적으로) 코드를 interleaving 방식으로 분할 실행함으로
+# 멀티 쓰레딩 비슷(?)하게 동작시킨다.
+
+'''
+import threading
+import json
+
+from config.DatabaseConfig import *   # 임포트는 함수 변수 클래스
+from utils.Database import Database
+from utils.Botserver import Botserver
+from utils.Preprocess import Preprocess
+from models.intent.IntentModel import IntentModel
+from models.ner.NerModel import NerModel
+from utils.Findanswer import Findanswer
+
+# 전처리 객체 생성
+p = Preprocess(word2index_dic='train_tools/dict/chatbot_dict.bin',
+               userdic='utils/user_dic.tsv')
+
+# 의도 파악 모델
+intent = IntentModel(model_name='models/intent/intent_model.h5', preprocess=p)
+
+# 개체명 인식 모델
+ner = NerModel(model_name='models/ner/ner_model.h5', preprocess=p)
+
+# 클라이언트 요청을 수행하는 쓰레드(에 담을) 함수
+def to_client(conn, addr, params):
+    db = params['db']
+
+    try:
+        db.connect()
+
+        # 데이터 수신
+        read = conn.recv(2048)   # 수신 데이터가 있을 때까지 블로킹(대기) / 최대 2048 버퍼에 담음
+        print('===========================')
+        print('Connection from: %s' % str(addr))
+
+        if read is None or not read:
+            # 클라이언트 연결이 끊어지거나, 오류가 있는 경우
+            print('클라이언트 연결 끊어짐')
+            exit(0)     # Thread 종료
+
+        # json 데이터로 변환
+        recv_json_data = json.loads(read.decode())   #파이썬객체로만들어주는 작업
+        print("데이터 수신 : ", recv_json_data)
+        query = recv_json_data['Query']    # 클라이언트로부터 전송된 질의어
+
+
+        #=================================
+        # decode: (encoding: str = ..., errors: str = ...) -> str
+        # Decode the bytes using the codec registered for encoding.
+
+        # encoding
+        # The encoding with which to decode the bytes.
+        #=================================
+
+
+        # 파이썬으로 지금 클라이언트의 질의어를 받앗다? 그러면~
+
+        # 의도 파악
+        intent_predict = intent.predict_class(query)
+        intent_name = intent.labels[intent_predict]
+
+        # 개체명 파악
+        ner_predicts = ner.predict(query)
+        ner_tags = ner.predict_tags(query)
+
+        # 답변 검색
+        try:
+            f = Findanswer(db)
+            answer_text, answer_image = f.search(intent_name, ner_tags)
+            answer = f.tag_to_word(ner_predicts, answer_text)
+
+        except:
+            answer = "죄송해요 무슨 말인지 모르겠어요. 조금 더 공부 할게요."
+            answer_image = None
+
+        sent_json_data_str = {    # response 할 JSON 데이터를 준비할 겁니다~
+            "Query" : query,
+            "Answer": answer,
+            "AnswerImageUrl" : answer_image,
+            "Intent": intent_name,
+            "NER": str(ner_predicts)
+        }
+
+        message = json.dumps(sent_json_data_str)
+        print("=++++++++++++++++++")
+        print("message type:",type(message))
+        print(message)
+        print("=++++++++++++++++++")
+        conn.send(message.encode())    # resoponse 
+
+
+    except Exception as ex:
+        print(ex)
+    
+    finally:
+        if db is not None:
+            db.close()
+        conn.close()        # 응답이 끝나면 클라이언트와의 연결(클라이언트 소켓) 도 close 해야 한다.
+
+
+if __name__ == '__main__':
+
+    # 질문/답변 학습 디비 연결 객체 생성
+    db = Database(
+        host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db_name=DB_NAME
+    )
+    print("DB 접속")
+
+
+    # ① 챗봇 소켓 서버 생성
+    port = 5050     # 서버의 통신포트
+    listen = 100    # 최대 클라이언트 연결수
+
+    # 봇 서버 동작
+    bot = Botserver(port, listen)
+    bot.create_sock()
+    print('bot start')
+
+    while True:
+        conn, addr = bot.ready_for_client()   # 클라이언트가 요청이 올때까지 리스닝함(대기) 하다가 연결 수락되면~! 저 2개 리턴
+
+        params = {
+            "db": db,
+        }
+
+
+        # 요청이 올때마다 쓰레드 하나 보내고, 또들어오면 또보내고 또보내고.....While True (무한 루트)
+        client = threading.Thread(target=to_client, args=(conn, addr, params))   # to_client로 저변수들이 넘어간다~
+        client.start()     # 쓰레드 시작
+
+
+
+
+
